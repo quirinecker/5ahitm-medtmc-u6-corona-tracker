4a446e0a39488af4ac796803fed2df3b5a3bfd9c
QuirinEcker <quirin.ecker@bajupa.com>
2022-01-07 12:45:56 +0100

    fixed weird moving adapter is now in the package and listview class is in the root package

--- app/src/main/java/com/example/coronatesttracker/CoronaTestListFragment.kt
+++ app/src/main/java/com/example/coronatesttracker/CoronaTestListFragment.kt
@@ -1,11 +1,10 @@
-package com.example.coronatesttracker.adapter
+package com.example.coronatesttracker
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.coronatesttracker.R
 import com.example.coronatesttracker.databinding.FragmentCoronaTestListBinding
 import com.example.coronatesttracker.model.CoronaTest
 
---
+++ app/src/main/java/com/example/coronatesttracker/adapter/CoronaTestListAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.coronatesttracker.adapter
+
+class CoronaTestListAdapter {
+}
