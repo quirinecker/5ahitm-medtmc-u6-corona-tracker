4530ad4167f6270f8066449a7d7e5a35a54b248c
QuirinEcker <quirin.ecker@bajupa.com>
2022-01-11 09:34:58 +0100

    moved to new NavigationViewContainer

--- app/src/main/java/com/example/coronatesttracker/MainActivity.kt
+++ app/src/main/java/com/example/coronatesttracker/MainActivity.kt
@@ -2,16 +2,24 @@ package com.example.coronatesttracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val navController = this.findNavController(R.id.nav_host_fragment)
-        NavigationUI.setupActionBarWithNavController(this, navController)
+        val navGraph = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navGraph.navController
+       setupActionBarWithNavController(this, navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
